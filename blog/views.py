05d515437510534feb13f64664665da7873842b7
olya <olianine@gmail.com>
2020-01-21 12:46:47 -0500

    Added page SIGNIN.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+def signin(request):
+    return render(request, 'blog/signin.html', {})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
